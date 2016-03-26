f2ee48c2b351e54a3604fd394f75865a8222cc9c
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-26 16:25:15 -0700

    add new index redirect route for dashboard

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import { Route, IndexRoute, IndexRedirect } from 'react-router'
 import App from './components/app'
 import Messages from './containers/messages'
 import Finances from './containers/finances'
 import Chores from './containers/chores'
+import Dashboard from './containers/dashboard'
 
 export default (
   <Route path='/' component={App}>
+    <IndexRedirect from='/' to='/dashboard' />
+    <Route path='/dashboard' component={Dashboard} />
     <Route path='finances' component={Finances} />
     <Route path='messages' component={Messages} />
     <Route path='chores' component={Chores} />
