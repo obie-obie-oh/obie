7c0c959e57bf6095e17f0b23f56a94eb74054443
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-26 17:50:19 -0700

    set up basic server just for photo upload

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Dropzone from 'react-dropzone'
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+    this.onDrop.bind(this)
+  }
+
+  onDrop(files) {
+    console.log('got the files ', files)
+  }
+
   render() {
     return (
       <main className='dashboard'>
         Dashboard
+        <Dropzone onDrop={this.onDrop}>
+          <div>Drop a file here or click to select files to upload</div>
+        </Dropzone>
       </main>
     )
   }
