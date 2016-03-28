ed3c28cfad1b11c6c4ccf1b4f2e93d596c4085cc
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-28 16:38:58 -0700

    abstract action types to types.js file in actions folder.

---
+++ src/actions/types.js
@@ -0,0 +1,5 @@
+export const FETCH_CHORES = 'fetch_chores'
+export const FETCH_MESSAGES = 'fetch_messages'
+export const FETCH_LANDLORD_MESSAGES = 'fetch_landlord_messages'
+export const FETCH_PAYMENTS = 'fetch_payments'
+export const FETCH_BILLS = 'fetch_bills'
