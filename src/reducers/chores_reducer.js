ebf3afd7dc382d488344bfc8ac7ab358698f9802
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-28 16:40:26 -0700

    refactor all calls for action types to refere to the types.js file, add finance reducer to root reducer

--- src/reducers/chores_reducer.js
+++ src/reducers/chores_reducer.js
@@ -1,9 +1,9 @@
-import {FETCH_CHORES} from '../actions'
+import { FETCH_CHORES } from '../actions/types'
 
 const INITIAL_STATE = []
 //research this
 
-export default function (state = INITIAL_STATE, action) {
+export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_CHORES:
       return [...state, ...action.payload.data]
