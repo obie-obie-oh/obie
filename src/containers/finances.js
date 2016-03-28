6546b2b945018cc68ba9dee44213938aab294f28
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-28 16:39:47 -0700

    add finance container and reducer to account for bills and payments on state

--- src/containers/finances.js
+++ src/containers/finances.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { fetchPayments } from '../actions'
 
 class Finances extends Component {
+  componentWillMount() {
+    this.props.fetchPayments()
+  }
+
   render() {
+    console.log('finances props', this.props)
     return (
       <article className="finances content-container">
         <p>finance component</p>
@@ -13,7 +19,10 @@ class Finances extends Component {
 
 
 function mapStateToProps (state) {
-
+  return {
+    bills: state.finances.bills,
+    payments: state.finances.payments
+  }
 }
 
-export default connect()(Finances)
+export default connect(mapStateToProps, {fetchPayments})(Finances)
