776f056320486e6aac222ef8d333391cf00e3652
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-04-02 00:03:43 -0700

    Removed duplicate comment from finance nav

--- src/components/finance_navbar/finance_navmenu.js
+++ src/components/finance_navbar/finance_navmenu.js
@@ -28,18 +28,3 @@ const FinanceNavmenu = () => {
 }
 
 export default FinanceNavmenu
-
-
-//The Style below is what I would like to use for this one but need to 
-//figure out how to use it with Link
-// <div className="btn-group" data-toggle="buttons">
-//   <label className="btn btn-primary-outline active">
-//     <input type="radio" name="options" id="option1" autocomplete="off" checked />Payments
-//   </label>
-//   <label className="btn btn-primary-outline">
-//     <input type="radio" name="options" id="option2" autocomplete="off" /> Bills
-//   </label>
-//   <label className="btn btn-primary-outline">
-//     <input type="radio" name="options" id="option3" autocomplete="off" /> Charge
-//   </label>
-// </div>
