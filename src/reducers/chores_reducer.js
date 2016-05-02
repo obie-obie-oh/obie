656c034dc9267f04c910a57fdf2ba519a8be881d
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-01 17:29:50 -0700

    fixed bug with chores repeating

--- src/reducers/chores_reducer.js
+++ src/reducers/chores_reducer.js
@@ -1,12 +1,11 @@
 import { FETCH_CHORES } from '../actions/types'
 
 const INITIAL_STATE = []
-//research this
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_CHORES:
-      return [...state, ...action.payload.data]
+      return action.payload.data
     default:
       return state
   }
