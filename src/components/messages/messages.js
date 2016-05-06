eacd8c9c66c75ee5eaf2d0649c9237727f33133a
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-06 13:27:22 -0700

    refactor messages component to now route to landlord vs roommate messages

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -2,37 +2,17 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import MessageEntry from './message_entry'
 import MessageForm from './message_form'
-import { fetchMessages } from '../../actions'
-import $ from 'jquery'
+import MessagesNavmenu from './messages_navmenu'
 
 class Messages extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentWillMount() {
-    this.props.fetchMessages()
-  }
-
   render() {
     return (
       <article className="messages content-container col-md-9">
-        <h2 className="text-center">Messages</h2>
-        <MessageForm />
-        <section className="message-list">
-          {this.props.messages.map((message, i) => 
-            <MessageEntry key={i} message={message} />
-          )}
-        </section>
+        <MessagesNavmenu />
+        {this.props.children}
       </article>
     )
   }
 }
 
-function mapStateToProps (state) {
-  return {
-    messages: state.messages
-  }
-}
-
-export default connect(mapStateToProps, {fetchMessages})(Messages)
+export default Messages
