4737c0e01bec1a896d721c92648a630024a8e2fc
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-06 13:25:16 -0700

    create roommate messages component

---
+++ src/components/messages/roommate_messages.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import MessageEntry from './message_entry'
+import { fetchMessages } from '../../actions'
+import MessageForm from './message_form'
+
+class RoommateMessages extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    this.props.fetchMessages()
+  }
+
+  render() {
+    return (
+      <article className="messages content-container col-md-9">
+        <MessageForm />
+        <section className="message-list">
+          {this.props.messages.map((message, i) => 
+            <MessageEntry key={i} message={message} />
+          )}
+        </section>
+      </article>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    messages: state.messages
+  }
+}
+
+export default connect(mapStateToProps, {fetchMessages})(RoommateMessages)
