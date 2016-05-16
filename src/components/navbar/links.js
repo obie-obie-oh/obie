748757b0e7b5c39735d91bd920bb7ff8fd174c62
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-15 23:10:29 -0700

    remove navmenu completely, create links.js to hold a constant list of links, map over links to create burger menu items vs navbar items instead of using navmenu to improve readability

---
+++ src/components/navbar/links.js
@@ -0,0 +1,16 @@
+const links = {
+  Dashboard: {
+    to: '/dashboard'
+  },
+  Messages: {
+    to: '/house/messages'
+  },
+  Finance: {
+    to: '/house/finances'
+  },
+  Todo: {
+    to: '/house/todos'
+  }
+}
+
+export default links
