3d51e61e88da58d156c23cb34c33952a887526d1
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-05-16 12:19:41 -0700

    Small style change

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("./style/style.css", {allChunks: false})
+    new ExtractTextPlugin('style/style.css', {allChunks: false})
   ],
   resolve: {
     extensions: ['', '.js', '.jsx']
