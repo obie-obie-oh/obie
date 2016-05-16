745be5e9a8894437e46d5b7fb2c011788287cf68
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-15 23:28:05 -0700

    due_date renders optionally on chore entries

--- src/components/chores/chore_entry.js
+++ src/components/chores/chore_entry.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import choreIcons from '../../assets/chore_icons'
+import h from '../../helpers'
 
 const ChoreEntry = ({
   chore
 }) => (
   <section className="chore-entry list-entry col-sm-12">
     <div className="col-xs-8">
+      {console.log(chore)}
       <div>
         <div className="caption">
           <h4 className="chore-name">{chore.chorename}</h4> 
-          <p>{chore.name} <span> {chore.dueDate} </span> {chore.details} </p>
+          <p>{chore.name} <span> {chore.details} </span> {chore.due_date ? h.getPaymentDate(chore.due_date) : ''} </p>
         </div>
       </div>
     </div>
