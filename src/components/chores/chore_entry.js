1832dde69ee68f148fc73635116d8dacfab32f3b
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-20 13:04:53 -0700

    complete todo items

--- src/components/chores/chore_entry.js
+++ src/components/chores/chore_entry.js
@@ -3,7 +3,8 @@ import choreIcons from '../../assets/chore_icons'
 import h from '../../helpers'
 
 const ChoreEntry = ({
-  chore
+  chore,
+  completeTodo
 }) => (
   <section className="chore-entry list-entry col-sm-12">
     <div className="col-xs-8">
@@ -15,7 +16,7 @@ const ChoreEntry = ({
       </div>
     </div>
     <div className="col-xs-4 flex-end-button">
-      <button className="btn btn-success-outline pull-right"><i className="fa fa-check" aria-hidden="true"></i></button>
+      <button className="btn btn-success-outline pull-right" onClick={() => completeTodo(chore.id)}><i className="fa fa-check" aria-hidden="true"></i></button>
     </div>
   </section>
 )
